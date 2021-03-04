1ea95a60525868f271d36015e065c80f1c9917b1
Mark Moawad <msmoawad99@gmail.com>
2021-03-03 21:24:41 -0500

    done with question 3

--- hw1.ml
+++ hw1.ml
@@ -7,3 +7,9 @@ let rec float_pow x n = if n = 0 then x else float_pow (x*.x) (n-1);;
 let rec compress = function
     | a :: (b :: _ as t) -> if a = b then compress t else a :: compress t
     | lst -> lst;;
+
+
+(*Question 3*)
+let rec remove_if lst f = match lst with
+| h::t -> if (f h) then remove_if t f else h::remove_if t f
+| [] -> [];;
