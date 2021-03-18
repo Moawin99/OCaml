04f93c0124ba81052d1bb44a7f7570a8ff8da3b8
Mark Moawad <msmoawad99@gmail.com>
2021-03-17 22:21:27 -0400

    done with 2.1

--- hw1.ml
+++ hw1.ml
@@ -90,3 +90,29 @@ let rec powerset lst =
     in match lst with
     | [] -> [[]]
     | h::t -> (powerset t) @ reduce h (powerset t);;
+
+
+(*Question 11*)
+type bool_expr = 
+    | Lit of string
+    | Not of bool_expr
+    | And of bool_expr * bool_expr
+    | Or of bool_expr * bool_expr;;
+
+let truth_table a b expr = 
+    let rec evaluate c c_val d d_val = function
+    | Lit x -> if x = c then c_val else if x = d then d_val else failwith "Invalid expression"
+    | Not n -> evaluate c c_val d d_val n
+    | And(a1, a2) -> evaluate c c_val d d_val a1 && evaluate c c_val d d_val a2
+    | Or(o1, o2) -> evaluate c c_val d d_val o1 || evaluate c c_val d d_val o2
+    in 
+    [(true, true, evaluate a true b true expr);
+     (true, false, evaluate a true b false expr);
+     (false, true, evaluate a false b true expr);
+     (false, false, evaluate a false b false expr)];;
+
+(*Question 12*)
+type expr =
+    | Const of int
+    | Var of string
+    | Plus 
